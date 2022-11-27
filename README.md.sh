1b8d9757b2f60690a1d40e2b77ffe2944e583038
Gabor Schulz <gabor@gaborschulz.com>
2022-11-27 17:15:44 +0100

    Added mypy and pylint badge to README.md

--- README.md.sh
+++ README.md.sh
@@ -4,7 +4,6 @@
 [![license: MIT](https://img.shields.io/badge/license-MIT-blue)](https://opensource.org/licenses/MIT)
 [![security: bandit](https://img.shields.io/badge/security-bandit-yellow.svg)](https://github.com/PyCQA/bandit)
 [![mypy: checked](https://img.shields.io/badge/mypy-checked-blue)](http://mypy-lang.org)
-[![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint)
 [![mypy: checked](https://img.shields.io/badge/mypy-checked-blue)](http://mypy-lang.org)
 [![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint)
 [![Tests](https://github.com/gaborschulz/ics-to-todoist/actions/workflows/pytest.yml/badge.svg)](https://github.com/gaborschulz/ics-to-todoist/actions/workflows/pytest.yml)
