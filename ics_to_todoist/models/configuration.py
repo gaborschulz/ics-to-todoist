e64ce0e2eed50c9b805c1fbd9acf55b6b5c24911
Gabor Schulz <gabor@gaborschulz.com>
2022-12-03 13:35:11 +0100

    ics-to-todoist-27 Raise exception if no todoist key is provided

--- ics_to_todoist/models/configuration.py
+++ ics_to_todoist/models/configuration.py
@@ -2,7 +2,7 @@ import os
 import re
 from zoneinfo import ZoneInfo
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, validator
 
 from .reminder_time import ReminderTime
 
@@ -13,21 +13,30 @@ def todoist_api_key_factory() -> str:
 
 
 class Configuration(BaseModel):
-    """ Configuration """
+    """Configuration"""
     relevant_names: str | None
     target_project: str
     default_reminder: bool = False
     timezone: str = 'UTC'
     reminder_times: list[ReminderTime]
-    todoist_api_key: str = Field(default_factory=todoist_api_key_factory)
+    todoist_api_key: str = ''
     only_future_events: bool = True
 
     @property
     def zoneinfo(self) -> ZoneInfo:
-        """ Get the timezone as a ZoneInfo object"""
+        """Get the timezone as a ZoneInfo object"""
         return ZoneInfo(self.timezone)
 
     @property
     def relevant_names_regex(self) -> re.Pattern:
-        """ Get relevant names as a compiled regex pattern """
+        """Get relevant names as a compiled regex pattern"""
         return re.compile(self.relevant_names)
+
+    @validator('todoist_api_key', always=True, pre=True)
+    def validate_todoist_api_key(cls, value: str) -> str:  # pylint: disable=no-self-argument
+        """Prefill todoist_api_key if not provided directly from TODOIST_API_KEY environment variable"""
+        if not value:
+            value = os.getenv('TODOIST_API_KEY', '')
+        if not value:
+            raise ValueError('TODOIST_API_KEY environment variable has to be set or the todoist_api_key field has to be provided')
+        return value
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -18,6 +18,17 @@ def configuration(monkeypatch):
     return config
 
 
+@pytest.fixture
+def configuration_no_timezone(monkeypatch):
+    monkeypatch.setenv("TODOIST_API_KEY", "TEST_KEY")
+
+    with open('data/test_config.toml', 'rb') as conf:
+        config_json = tomllib.load(conf)
+    config_json.pop('timezone')
+    config = Configuration(**config_json, )
+    return config
+
+
 def test_configuration_timezone(configuration):
     assert configuration.zoneinfo == ZoneInfo(configuration.timezone)
 
@@ -26,13 +37,19 @@ def test_relevant_names_regex(configuration):
     assert configuration.relevant_names_regex == re.compile(configuration.relevant_names)
 
 
-def test_todoist_api_key_factory_usage(monkeypatch, configuration):
+def test_todoist_api_key_env_var_usage(configuration):
     assert configuration.todoist_api_key == 'TEST_KEY'
 
 
-def test_default_timezone():
+def test_default_timezone(configuration_no_timezone):
+    assert configuration_no_timezone.timezone == 'UTC'
+
+
+def test_missing_todoist_api_key_raises_validation_error():
     with open('data/test_config.toml', 'rb') as conf:
         config_json = tomllib.load(conf)
-    config_json.pop('timezone')
-    config = Configuration(**config_json, )
-    assert config.timezone == 'UTC'
+
+        if 'todoist_api_key' in config_json:
+            config_json.pop('todoist_api_key')
+        with pytest.raises(Exception):
+            config = Configuration(**config_json, )
